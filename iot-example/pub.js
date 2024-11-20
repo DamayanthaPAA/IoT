f24d674485af98c06a0055bfd4103a2ea769ce36
Anura Damayantha <anura.icon.fi@gmail.com>
2024-11-20 08:41:42 +0200

    init

---
+++ iot-example/pub.js
@@ -0,0 +1,6 @@
+const url = 'tls:f0ae3f10c7934f22af58baf1325a7a00.s2.eu.hivemq.cloud:8883'
+const username = 'IoT-GUEST'
+const password = 'Gj@ci85TjdJvzMZ'
+const clientid = 'gateway-controller-001-reporter'
+const topic = '/karelia/wartsila/026a'
+
